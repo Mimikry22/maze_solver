ed1b8d3200b27e16afeccf81f3b46994d48bf082
Mimikry22 <kcm22@gmx.de>
2025-02-09 11:06:49 +0100

    Added automatic solver

--- maze.py
+++ maze.py
@@ -119,3 +119,46 @@ class Maze:
             for j in range(self._num_rows):
                 self._cells[i][j]._visited = False
 
+    def solve(self):
+        return self._solve_r(0,0)
+
+    def _solve_r(self,i,j):
+        self._animate()
+        self._cells[i][j]._visited = True
+        if i == self._num_cols-1 and j == self._num_rows-1:
+            return True
+        
+        # left
+        if i > 0:
+            if self._cells[i-1][j]._visited == False and self._cells[i][j].has_left_wall == False:
+                self._cells[i][j].draw_move(self._cells[i-1][j])
+                if self._solve_r(i-1,j):
+                    return True
+                self._cells[i][j].draw_move(self._cells[i-1][j], True)
+        # right
+        if i < self._num_cols -1 :
+            if self._cells[i+1][j]._visited == False and self._cells[i][j].has_right_wall == False:
+                self._cells[i][j].draw_move(self._cells[i+1][j])
+                if self._solve_r(i+1,j):
+                    return True
+                self._cells[i][j].draw_move(self._cells[i+1][j], True)
+
+        # up
+
+        if j > 0:
+            if self._cells[i][j-1]._visited == False and self._cells[i][j].has_top_wall == False:
+                self._cells[i][j].draw_move(self._cells[i][j-1])
+                if self._solve_r(i,j-1):
+                    return True
+                self._cells[i][j].draw_move(self._cells[i][j-1], True)
+
+        # down
+
+        if j < self._num_rows-1:
+            if self._cells[i][j+1]._visited == False and self._cells[i][j].has_bottom_wall == False:
+                self._cells[i][j].draw_move(self._cells[i][j+1])
+                if self._solve_r(i,j+1):
+                    return True
+                self._cells[i][j].draw_move(self._cells[i][j+1], True)
+        
+        return False
